3571a5293ac09a8aa2942db9b7556421b9365283
Daniel schwartz <mdblakestj@gmail.com>
2018-12-15 12:24:16 -0500

    Adding base project

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter.js'
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+// import IndecisionApp from './components/IndecisionApp.js;
+//import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+
+
+
+
+
+
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
